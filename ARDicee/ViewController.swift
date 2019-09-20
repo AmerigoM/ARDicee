468aef5d81b771ab95eee613d70e8830b500ffbe
rexam14 <amerigo.mancino@gmail.com>
2019-09-20 19:28:25 +0200

    Detect a plane in the scene.

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -18,6 +18,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // debug mode to show feature points
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+        
         // Set the view's delegate
         sceneView.delegate = self
         
@@ -46,16 +49,16 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // enable lights in the scene
         sceneView.autoenablesDefaultLighting = true
         
-        // Create a new scene
-        let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
-        
-        // search for a node in a certain scene and include all its subnodes (there's any at the moment)
-        if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
-            diceNode.position = SCNVector3(x: 0, y: 0, z: -0.1)
-            
-            // Set the scene to the view
-            sceneView.scene.rootNode.addChildNode(diceNode)
-        }
+//        // Create a new scene
+//        let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
+//
+//        // search for a node in a certain scene and include all its subnodes (there's any at the moment)
+//        if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
+//            diceNode.position = SCNVector3(x: 0, y: 0, z: -0.1)
+//
+//            // Set the scene to the view
+//            sceneView.scene.rootNode.addChildNode(diceNode)
+//        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -63,6 +66,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
+        
+        // add horizontal planes detection
+        configuration.planeDetection = .horizontal
 
         // Run the view's session
         sceneView.session.run(configuration)
@@ -73,6 +79,44 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Pause the view's session
         sceneView.session.pause()
+    }
+    
+    // MARK: - DELEGATE METHODS FOR ARSCNVIEW
+    
+    // a horizontal surface was detected and it's given to that surface (which is a
+    // AR anchor) a width and a height
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        // check if that anchor is a plane rather than some other 3D object in the scene
+        if anchor is ARPlaneAnchor {
+            // downcast the anchor to a plane anchor
+            let planeAnchor = anchor as! ARPlaneAnchor
+            
+            // create a plane object in SceneKit based on the planeAnchor detected
+            let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
+            
+            // create a plane node for the plane object
+            let planeNode = SCNNode()
+            planeNode.position = SCNVector3(x: planeAnchor.center.x, y: 0, z: planeAnchor.center.z)
+            
+            // planes are vertical by default so we have to rotate it by 90 degrees
+            planeNode.transform = SCNMatrix4MakeRotation(-Float.pi/2, 1, 0, 0)
+            
+            // grate a new texture material
+            let gridMaterial = SCNMaterial()
+            gridMaterial.diffuse.contents = UIImage(named: "art.scnassets/grid.png")
+            
+            // assign the matierial to the plane object
+            plane.materials = [gridMaterial]
+            
+            // set the node geometry to the plane in order to make it appear in the scene
+            planeNode.geometry = plane
+            
+            // add the child node into the root node: the parameter node is a blank node that
+            // got created when a new surface was detected
+            node.addChildNode(planeNode)
+        } else {
+            return
+        }
     }
 
 }
